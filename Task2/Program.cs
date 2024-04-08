cd1a0739d65b459321313c18483c7ffd6ca84d4b
Anna Suv <multguga@gmail.com>
2024-04-08 13:48:39 +0300

    Сложение и умножение

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿int i1 = 8;
+int i2 = 41;
+int sum = i1 + i2;
+System.Console.WriteLine(sum);
+
+
+long l1 = 6;
+long sum2 = i1 + l1;
+System.Console.WriteLine(sum2);
+
+double d1 = 5.5;
+double sum3 = d1 + i1;
+
+int produkt1 = i1 * i2;
+long produkt2 = l1 * i2;
+double produkt = i1 * d1;
+System.Console.WriteLine(produkt1);
+System.Console.WriteLine(produkt2);
+System.Console.WriteLine(produkt);
