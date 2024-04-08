e6ea577918b245d1cfa27c70a57cb8a4d6d0dc33
Anna Suv <multguga@gmail.com>
2024-04-08 16:32:52 +0300

    Задание про приветсвие любимчика

---
+++ ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username == "Dasha")
+{
+    Console.WriteLine("Ура, это же Даша!");
+}
+else
+{
+    Console.WriteLine("Привет, ");
+    Console.Write(username);
+    }
