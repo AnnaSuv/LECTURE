ebbfb6b53c73ea672a5748dd6c6b247d0b2e1b65
Anna Suv <multguga@gmail.com>
2024-04-09 10:31:09 +0300

    Задача про вывод чётных чисел в диапазоне

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,11 +2,19 @@
 
 
 int value = 100;
-Console.WriteLine (value);
+Console.WriteLine(value);
 
 double d = 5.55;
 bool b = true;
-Console.WriteLine (d);
-Console.WriteLine (b);
+Console.WriteLine(d);
+Console.WriteLine(b);
 char c = '!';
-Console.WriteLine (c);
+Console.WriteLine(c);
+
+int number = 5;
+int i = 2;
+while (i <= number)
+{
+    System.Console.WriteLine(i + " ");
+    i = i + 2;
+}
